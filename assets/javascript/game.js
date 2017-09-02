71952fdbe90e010793d1a47873d9e24094abc4ee
Stella U <stellau@MeowBook-Pro.local>
2017-09-02 07:38:40 -0700

    updates

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,50 +1,48 @@
 $(document).ready(function(){
-//variables
-var wins = 0;
-var losses = 0;
-var drEvilWants = "";
-var userScore = 0;
-
-//The random number shown at the start of the game should be between 19 - 120.
-//computer randomize Dr Evil wants
-var drEvilRandomize = Math.floor(Math.random()*101+19)
-console.log(drEvilRandomize);
-$('#drEvilWants').text(drEvilRandomize);
-
-//Each cat should have a random hidden value between 1 - 12.
-var randomCat1= Math.floor(Math.random()*11+1)
-var randomCat2= Math.floor(Math.random()*11+1)
-var randomCat3= Math.floor(Math.random()*11+1)
-var randomCat4= Math.floor(Math.random()*11+1)
-
-function winPOP() {
-	alert("It's your lucky day! Dr Evil(& your cats) spare your life!");
-	wins ++;
-	$('#winCount').text(wins);
-	reset();
-}
-
-function lossPOP() {
-	alert("Too many cats for Dr Evil. You are dead!");
-	losses ++;
-	$('#lossCount').text(losses);
-	reset();
-}
-
-function reset(){
 	userScore = 0;
-	randomCat1= Math.floor(Math.random()*11+1);
-	randomCat2= Math.floor(Math.random()*11+1);
-	randomCat3= Math.floor(Math.random()*11+1);
-	randomCat4= Math.floor(Math.random()*11+1);
-	$('#catWorth').text(userScore);
+	var wins = 0;
+	var losses = 0;
+	var drEvilWants = "";
 
-	drEvilRandomize = Math.floor(Math.random()*101+19)
+	//The random number shown at the start of the game should be between 19 - 120.
+	var drEvilRandomize = Math.floor(Math.random()*101+19)
 	console.log(drEvilRandomize);
 	$('#drEvilWants').text(drEvilRandomize);
-}
 
-//creat value for each cat
+	//Each cat should have a random hidden value between 1 - 12.
+	var randomCat1= Math.floor(Math.random()*11+1)
+	var randomCat2= Math.floor(Math.random()*11+1)
+	var randomCat3= Math.floor(Math.random()*11+1)
+	var randomCat4= Math.floor(Math.random()*11+1)
+
+	function winPOP() {
+		alert("It's your lucky day! Dr Evil(& your cats) spare your life!");
+		wins ++;
+		$('#winCount').text(wins);
+		reset();
+	}
+
+	function lossPOP() {
+		alert("Too many cats for Dr Evil. You are dead!");
+		losses ++;
+		$('#lossCount').text(losses);
+		reset();
+	}
+
+	function reset(){
+		userScore = 0;
+		randomCat1= Math.floor(Math.random()*11+1);
+		randomCat2= Math.floor(Math.random()*11+1);
+		randomCat3= Math.floor(Math.random()*11+1);
+		randomCat4= Math.floor(Math.random()*11+1);
+		$('#catWorth').text(userScore);
+
+		drEvilRandomize = Math.floor(Math.random()*101+19)
+		console.log(drEvilRandomize);
+		$('#drEvilWants').text(drEvilRandomize);
+	}
+
+//game run
 	$('#cat1').click(function(){
 	    userScore = userScore + randomCat1;
 	    console.log(userScore);
